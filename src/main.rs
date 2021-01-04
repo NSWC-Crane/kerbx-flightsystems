36caebbda11163a0eba4fd60064de458620a0e2e
Dr. Whom Ph.D <drwhom@outlook.com>
2021-01-04 13:03:27 -0500

    Switched from ncurses to pancurses due to better API.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use nalgebra::{Vector3, Rotation3, Rotation};
-use ncurses::*;
 use krpc::{rpc};
+use pancurses::{initscr, endwin, noecho, Input};
 
 fn main() {
-    initscr();
-    raw();
+    let main_window = initscr();
 
+    // Window constants used later for printing text    
+    let lines = main_window.get_max_y();
+    let cols = main_window.get_max_x();
 
-    println!("Hello, world!");
+    // Display our initial layout
+    main_window.printw("Kerbel File Control");
+    main_window.mvaddstr(lines - 1, 0, "Press 'q' to quit");
+    main_window.refresh();
+    noecho();
+
+    // Connect to KSP via krpc-rs
+
+    // Main loop for handling information from ksp
+    loop {
+        if let Some(Input::Character('q')) = main_window.getch() {
+            break;
+        }
+    }
+
+    // Cleanup after ourselves
+    endwin();
 }
