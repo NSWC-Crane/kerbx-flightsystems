273a4cf398c4d6e978bf204e9b1dbfeff262cbfb
Dr. Whom Ph.D <drwhom@outlook.com>
2021-01-08 13:38:59 -0500

    Implemented calculations for lat, lon, altitude, and pitch. Roll, pitch, heading numbers still don't match the Python outputs.

--- src/main.rs
+++ src/main.rs
@@ -38,15 +38,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         let vessel = client.mk_call(&space_center::get_active_vessel())?;
         
         let surf_ref_frame = client.mk_call(&vessel.get_surface_reference_frame())?;
-        let orb_ref_frame = client.mk_call(&vessel.get_orbital_reference_frame())?;
         let vessel_ref_frame = client.mk_call(&vessel.get_reference_frame())?;
+
+        // Get values for calculating lat/lon
+        let orb_ref_frame = client.mk_call(&vessel.get_orbital_reference_frame())?;
+        let orbit = client.mk_call(&vessel.get_orbit())?;
+        let planet = client.mk_call(&orbit.get_body())?;
 
         // Get current vessel direction
         let direction = client.mk_call(&vessel.direction(&surf_ref_frame))?;
         let direction = Vector3::new(direction.0, direction.1, direction.2);
-
-        // Get current vessel position
-        let position = client.mk_call(&vessel.position(&orb_ref_frame))?;
 
         // Constants leveraged for the final calculation.
         let horizon = Vector3::new(0.0, direction[1], direction[2]);
@@ -54,24 +55,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         let up = Vector3::new(1.0, 0.0, 0.0);
 
         // This line is causing Protobuf(WireError(UnecpectedEof)) errors.
-        //let vessel_up = client.mk_call(&space_center::transform_direction((0.0, 0.0, -1.0), &vessel_ref_frame, &surf_ref_frame));
+        let vessel_up = client.mk_call(&space_center::transform_direction((0.0, 0.0, -1.0), &vessel_ref_frame, &surf_ref_frame))?;
 
         // Calculate pitch
         let pitch = if direction[0] < 0.0 {
-            -nalgebra::angle_between(&direction, &horizon)
+            -nalgebra::angle_between(&horizon, &direction)
         } else {
-            nalgebra::angle_between(&direction, &horizon)
+            nalgebra::angle_between(&horizon, &direction)
         };
         
         // Calculate heading
         let heading = if horizon[2] < 0.0 {
-            360.0 - nalgebra::angle_between(&north, &horizon)
+            360.0 - nalgebra::angle_between(&horizon, &north)
         } else {
-            nalgebra::angle_between(&north, &horizon)
+            nalgebra::angle_between(&horizon, &north)
         };
 
-        // Calculate roll -- can't calculate as vessel_up returns an error
-        /*
+        let vessel_up = Vector3::new(vessel_up.0, vessel_up.1, vessel_up.2);
         let plane_normal = nalgebra::cross(&direction, &up);
         let roll = nalgebra::angle_between(&vessel_up, &plane_normal);
         let roll = if up[0] > 0.0 {
@@ -83,12 +83,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                 roll - 180.0
             }
         };
-        */
+
+
+        // Get current vessel position
+        let position = client.mk_call(&vessel.position(&orb_ref_frame))?;
+        let lat = client.mk_call(&planet.latitude_at_position(position, &orb_ref_frame))?;
+        let lon = client.mk_call(&planet.longitude_at_position(position, &orb_ref_frame))?;
+        let alt = client.mk_call(&planet.altitude_at_position(position, &orb_ref_frame))?;
         
-        output_window.mvaddstr(1, 1, format!("Pitch: {}", pitch));
-        // output_window.mvaddstr(2, 1, format!("Roll: {}", roll));
-        output_window.mvaddstr(3, 1, format!("Heading: {}", heading));
-        //output_window.mvaddstr(1, 1, format!("Direction: {:?}", direction));
+        output_window.mvaddstr(1, 1, format!("Yaw: {:?}", heading));
+        output_window.mvaddstr(2, 1, format!("Pitch: {}", pitch));
+        output_window.mvaddstr(3, 1, format!("Roll: {}", roll));
+        output_window.mvaddstr(4, 1, format!("Lat: {}", lat));
+        output_window.mvaddstr(5, 1, format!("Lon: {}", lon));
+        output_window.mvaddstr(6, 1, format!("Alt: {}", alt));
+        output_window.mvaddstr(7, 1, format!("Direction: {:?}", direction));
+
         if let Some(Input::Character('q')) = main_window.getch() {
             break;
         }
