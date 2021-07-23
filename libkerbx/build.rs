47e8e72e6659f2b76e79b62b2d62146e1402b2e5
Dr. Whom, Ph.D <drwhom@outlook.com>
2021-07-23 14:49:27 -0400

    Added protobuf compilation to build script.

--- libkerbx/build.rs
+++ libkerbx/build.rs
@@ -1,14 +1,28 @@
 //use glob;
 use krpc_mars_terraformer;
+use protoc_rust;
 
-fn main () {
-	// Tell cargo to re-run this script only when json files in services/
-	// have changed. You can choose to omit this step if you want to
-	// re-generate services every time.
-	for path in glob::glob("services/*.json").unwrap().filter_map(Result::ok) {
-		println!("cargo:rerun-if-changed={}", path.display());
-	}
+fn main() {
+    // Tell cargo to re-run this script only when json files in services/
+    // have changed. You can choose to omit this step if you want to
+    // re-generate services every time.
+    for path in glob::glob("services/*.json")
+        .unwrap()
+        .filter_map(Result::ok)
+    {
+        println!("cargo:rerun-if-changed={}", path.display());
+    }
 
-	krpc_mars_terraformer::run("services/", "src/")
-		.expect("Could not terraform Mars :(")
+    krpc_mars_terraformer::run("services/", "src/").expect("Could not terraform Mars :(");
+
+    // Generate our protobuf files
+    println!("cargo:rerun-if-changed=protos/kerbx.proto");
+    println!("cargo:rerun-if-changed=src/kerbx.rs");
+
+    protoc_rust::Codegen::new()
+        .out_dir("src/")
+        .inputs(&["protos/kerbx.proto"])
+        .include("protos")
+        .run()
+        .expect("RUnning protoc failed.");
 }
