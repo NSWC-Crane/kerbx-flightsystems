f971e77d60ea39e724d9cd37f234d7529222542c
Dr. Whom, Ph.D <drwhom@outlook.com>
2021-09-20 09:45:49 -0400

    Created flight plan API for loading of kerbx flight plans.

---
+++ libkerbx/src/flightplan.rs
@@ -0,0 +1,20 @@
+use crate::kerbx::FlightPlan;
+use kerbx::FlightPlan;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
+use std::fs::File;
+use std::io::BufReader;
+
+//TODO: Handling with proper custom Error type
+pub fn load_from_string(flightplan: String) -> FlightPlan {
+    serde_json::from_str(flightplan.as_str()).expect("Error loading flight plan.")
+}
+
+//TODO: Handling with proper custom Error type
+/// Loads a flight plan from a specified file
+/// filename: Full canonical path to the flightplan to load
+pub fn load_from_file(filename: String) -> FlightPlan {
+    let mut file = File::open(filename);
+
+    serde_json::from_reader(BufReader::new(file)).expect("Error loading flight plan from file.")
+}
