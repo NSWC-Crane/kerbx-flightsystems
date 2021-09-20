e2e43d96c0ee485a2cce79b23664f3b6d281b185
Dr. Whom, Ph.D <drwhom@outlook.com>
2021-09-20 11:37:28 -0400

    Added api call to write flightplan to a file.

--- libkerbx/src/flightplan.rs
+++ libkerbx/src/flightplan.rs
@@ -2,7 +2,7 @@ use crate::kerbx::FlightPlan;
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 
 //TODO: Handling with proper custom Error type
 pub fn load_from_string(flightplan: String) -> FlightPlan {
@@ -16,4 +16,13 @@ pub fn load_from_file(filename: String) -> FlightPlan {
     let mut file = File::open(filename).expect("Could not open flight plan file.");
 
     serde_json::from_reader(BufReader::new(file)).expect("Error loading flight plan from file.")
+}
+
+/// Writes a flight plan from memory to specified file.
+/// filename: File to write to.
+/// &plan: Reference to flight plan object in memory
+pub fn write_to_file(filename: String, plan: &FlightPlan) {
+    let mut file = File::open(filename).expect("Could not open flight plan file.");
+
+    serde_json::to_writer(BufWriter::new(file), &plan);
 }
