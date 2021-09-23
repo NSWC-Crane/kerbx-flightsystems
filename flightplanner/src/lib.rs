449e27617bf65f026bdfaf4c7751b54cd873698b
Dr. Whom, Ph.D <drwhom@outlook.com>
2021-09-23 12:19:57 -0400

    Added public domain NOTICE.

--- flightplanner/src/lib.rs
+++ flightplanner/src/lib.rs
@@ -1,3 +1,26 @@
+/*
+* =================================================================================================
+*
+*                                      PUBLIC DOMAIN NOTICE
+*                           Naval Surface Warfare Center - Crane Division
+*
+*  This software is a "United States Government Work" under the terms of the United States
+*  Copyright Act. It was written as part of the author's official duties as a United States
+*  Government employee and thus cannot be copyrighted. This software/database is freely available
+*  to the public for use. Naval Surface Warfare Center - Crane Division (NSWC-CD) and the U.S.
+*  Government have not places any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy and reliability of the
+*  software and data, NSWC-CD and the U.S. Government do not and cannot warrant the performance or
+*  results that may be obtained by using this software or data. NSWC-CD and the U.S. Government
+*  disclaim all warranties, express or implied, including warranties of performance,
+*  merchantability or fitness for any particular purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* =================================================================================================
+*/
+
 use libkerbx::kerbx::Sheath_oneof_message::flightplan;
 use libkerbx::kerbx::*;
 use protobuf::{CodedInputStream, CodedOutputStream, ProtobufError, ProtobufResult};
